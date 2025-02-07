d1e0f86aa54db5c6189ebab3d187c9b5c6d911d0
Osuji Chioma Sarah <osujisarah@gmail.com>
2025-02-07 02:36:31 +0100

    Remove redundant getUserDashboard logic and modify getUserById.

--- controllers/getUserById.js
+++ controllers/getUserById.js
@@ -4,7 +4,12 @@ const getUserById = async (req, res) => {
   const { user_id } = req.params;
   try {
     const profile = await getJobSeekerById(user_id);
-    return res.json(profile);
+
+    if (profile.length === 0) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    return res.json(profile[0]);
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken");
-const getUserDashboard = require("../controllers/getUserDashboard");
 const getAllUsers = require("../controllers/getAllUsers");
 const getUserById = require("../controllers/getUserById");
 const updateUserProfile = require("../controllers/updateUserProfile");
 const deleteUserProfile = require("../controllers/deleteUserProfile");
 
-router.get("/dashboard", verifyToken, getUserDashboard);
-router.get("/users", getAllUsers);
+router.get("/users", verifyToken, getAllUsers);
 router.get("/:user_id", getUserById);
 router.patch("/dashboard/:user_id", updateUserProfile);
 router.delete("/dashboard/:user_id", deleteUserProfile);
